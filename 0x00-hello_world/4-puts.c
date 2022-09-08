08af3b00d1b64be3ec6967d25dfc41b0a8439ffa
Amachreech <amachreech@gmail.com>
2022-09-08 16:28:32 -0700

    4
    /
    '

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 /**
- * main - point to enter print
- * 
- * Return: 0 always after print
- */
+* main - point to enter print
+* 
+* Return: 0 always after print
+*/
 int main(void)	
 {puts("\"Programming is like building a multilingual puzzle");
 	return (0);
