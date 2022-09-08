18bbe31b3e78dddf0ba03765cf7ffb3b4f55826a
Amachreech <amachreech@gmail.com>
2022-09-08 14:27:17 -0700

    tasks 3-6

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,23 @@
+nclude <stdio.h>
+
+
+
+/**
+ *
+ * * main - Prints text
+ *
+ * *
+ *
+ * * Return: Always 0 (Successful)
+ *
+ * */
+
+int main(void)
+
+{
+
+	printf("with proper grammar, but the outcome is a piece of
+
+			art,\n");
+
+	return (0);
