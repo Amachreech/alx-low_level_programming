6cb9ebdff99024f6757faef0c0d62ad2dac11293
Amachreech <amachreech@gmail.com>
2022-09-13 16:46:30 -0700

    task 7 and 9

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  *print_last_digit - Prints the last digit of a number.
  *@n: The number in question.
@@ -11,5 +12,6 @@ int print_last_digit(int n)
 	if (last_digit < 0);
 		last_digit *= -1;
 	_putchar(last_digit + '0');
+
 	return (last_digit);
 }
--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -26,5 +26,3 @@ void times_table(void)
 
 		_putchar('\n');
 	}
-
-
